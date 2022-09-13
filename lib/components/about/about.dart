dc9555fdc2367fd6ba102ea3d0467931ff1e6f20
nguyen-thinh109 <thinh.nguyen.duc1@vdigital.vn>
2022-09-13 13:51:22 +0700

    update 1309

--- lib/components/about/about.dart
+++ lib/components/about/about.dart
@@ -9,7 +9,7 @@ class About extends StatelessWidget {
     final authorInfo = {
       "name": "Thịnh Nguyễn",
       "email": "nd.thinh91@gmail.com",
-      "phone": "098401891"
+      "phone": "0984018891"
     };
 
     final SpringController springController =

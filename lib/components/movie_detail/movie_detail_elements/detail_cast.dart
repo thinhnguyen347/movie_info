1ebc4b15ed6486f1fbfc933ed7ceaeb3987b36db
nguyen-thinh109 <thinh.nguyen.duc1@vdigital.vn>
2022-09-13 14:33:48 +0700

    update 1309

--- lib/components/movie_detail/movie_detail_elements/detail_cast.dart
+++ lib/components/movie_detail/movie_detail_elements/detail_cast.dart
@@ -47,17 +47,15 @@ Widget castItem({required Cast cast}) {
       margin: const EdgeInsets.only(right: 10),
       child: Column(children: [
         Expanded(
-            flex: 1,
-            child: ClipRRect(
+          flex: 1,
+          child: ClipRRect(
               borderRadius: BorderRadius.circular(10),
               clipBehavior: Clip.antiAlias,
               child: cast.profilePath != null
                   ? Image.network(
                       'https://image.tmdb.org/t/p/w500${cast.profilePath}')
-                  : ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: Image.asset('assets/images/noImg.jpg', width: 80)),
-            )),
+                  : Image.asset('assets/images/noImg.jpg')),
+        ),
         const SizedBox(height: 8),
         SizedBox(
             height: 40,

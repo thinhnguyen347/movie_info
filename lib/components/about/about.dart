2a473d751ccb5f825709f40f0c3b2f1738423436
nguyen-thinh109 <thinh.nguyen.duc1@vdigital.vn>
2022-09-13 10:06:34 +0700

    update 1309

--- lib/components/about/about.dart
+++ lib/components/about/about.dart
@@ -7,7 +7,7 @@ class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final authorInfo = {
-      "name": "Nguyen Duc Thinh",
+      "name": "Thịnh Nguyễn",
       "email": "nd.thinh91@gmail.com",
       "phone": "098401891"
     };
